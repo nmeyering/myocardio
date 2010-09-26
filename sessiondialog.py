498445008240e0a57c0bcee4ca620ba69409242d
Niels Meyering <nmeyering@gmail.com>
2010-09-26 22:49:12 +0200

    can now add exercises to workout listing etc.

--- sessiondialog.py
+++ sessiondialog.py
@@ -1,7 +1,9 @@
 # coding=utf-8
-from PyQt4 import QtGui
-from PyQt4.QtGui import QDialog
+from PyQt4 import QtGui, QtCore
+from PyQt4.QtGui import QDialog, QTableWidget, QTableWidgetItem
+from PyQt4.QtCore import QVariant
 from ui_sessiondialog import Ui_Dialog
+import sqlite3
 
 class SessionDialog( QDialog ):
 	def __init__( self ):
@@ -12,11 +14,43 @@ class SessionDialog( QDialog ):
 
 		self.ui.cancelButton.clicked.connect( self.reject )
 		self.ui.okButton.clicked.connect( self.accept )
+		self.ui.addButton.clicked.connect( self.addExerciseItem )
+		self.ui.deleteButton.clicked.connect( self.deleteItem )
 
-		self.ui.exerciseComboBox.addItems(
-			[
-				'Butterfly',
-				'Liegestütz'
-			]
-		)
+		self.cb = self.ui.exerciseComboBox
+		self.sb = self.ui.weightSpinBox
 
+		self.table = self.ui.exerciseTable
+
+		self.con = sqlite3.connect("data/db")
+		self.con.isolation_level = None
+		self.cur = self.con.cursor()
+
+		self.dbExercises()
+
+	def insertItem( self, item ):
+		row = self.table.rowCount()
+		self.table.insertRow( row )
+
+		for i in range( len( item ) ):
+			self.table.setItem(
+				row,
+				i,
+				QTableWidgetItem( item[i] )
+			)
+
+	def addExerciseItem( self ):
+		res = []
+		res.append( self.cb.itemData( self.cb.currentIndex() ).toString() )
+		res.append( self.cb.currentText() )
+		res.append( str( self.sb.value() ) )
+		self.insertItem( res )
+
+	def deleteItem( self ):
+		pass
+	
+	def dbExercises( self ):
+		self.cur.execute("SELECT * FROM 'exercise' ORDER BY 'name';")
+		exercises = self.cur.fetchall()
+		for e in exercises:
+			self.cb.addItem( str( e[1] ), e[0] )
